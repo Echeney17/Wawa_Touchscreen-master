0afdcfcb907aab802ed95047b03a84aba9b655bd
Echeney17 <48723154+Echeney17@users.noreply.github.com>
2020-05-01 14:27:38 -0400

    Add files via upload

--- Wawa_Touchscreen/Wawa_Touchscreen-master/Wawa_Touchscreen/Finished.xaml.cs
+++ Wawa_Touchscreen/Wawa_Touchscreen-master/Wawa_Touchscreen/Finished.xaml.cs
@@ -23,5 +23,10 @@ namespace Wawa_Touchscreen
         {
             InitializeComponent();
         }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
